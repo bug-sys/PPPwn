18a7bd55dabf50001217a7ab868cd6925f9bd5b9
SUIJUNG <40689080+SUIJUNG@users.noreply.github.com>
2024-05-09 21:29:18 +0700

    Update pppwn.py

--- pppwn.py
+++ pppwn.py
@@ -824,8 +824,8 @@ def main():
                             '1050', '1070', '1071', '1100'
                         ],
                         default='1100')
-    parser.add_argument('--stage1', default='stage1/stage1.bin')
-    parser.add_argument('--stage2', default='stage2/stage2.bin')
+    parser.add_argument('--stage1', default='/root/PPPwn/stage1/stage1.bin')
+    parser.add_argument('--stage2', default='/root/PPPwn/stage2/stage2.bin')
     args = parser.parse_args()
 
     print('[+] PPPwn - PlayStation 4 PPPoE RCE by theflow')
