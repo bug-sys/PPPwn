dfe6a62a36ab366190414fdea69a2495b1850e26
SUIJUNG <40689080+SUIJUNG@users.noreply.github.com>
2024-05-15 05:46:37 +0700

    Update pppwn.py

--- pppwn.py
+++ pppwn.py
@@ -827,15 +827,7 @@ class Exploit():
 def main():
     parser = ArgumentParser('pppwn.py')
     parser.add_argument('--interface', required=True)
-    parser.add_argument('--fw',
-                        choices=[
-                            '750', '751', '755',
-                            '800', '801', '803', '850', '852',
-                            '900', '903', '904', '950', '951', '960',
-                            '1000', '1001', '1050', '1070', '1071',
-                            '1100'
-                        ],
-                        default='1100')
+    parser.add_argument('--fw', default='1100')
     parser.add_argument('--stage1', default='/root/PPPwn/stage1/stage1.bin')
     parser.add_argument('--stage2', default='/root/PPPwn/stage2/stage2.bin')
     args = parser.parse_args()
@@ -849,23 +841,7 @@ def main():
     with open(args.stage2, mode='rb') as f:
         stage2 = f.read()
 
-    if args.fw in ('750', '751', '755'):
-        offs = OffsetsFirmware_750_755()
-    elif args.fw in ('800', '801', '803'):
-        offs = OffsetsFirmware_800_803()
-    elif args.fw in ('850', '852'):
-        offs = OffsetsFirmware_850_852()
-    elif args.fw == '900':
-        offs = OffsetsFirmware_900()
-    elif args.fw in ('903', '904'):
-        offs = OffsetsFirmware_903_904()
-    elif args.fw in ('950', '951', '960'):
-        offs = OffsetsFirmware_950_960()
-    elif args.fw in ('1000', '1001'):
-        offs = OffsetsFirmware_1000_1001()
-    elif args.fw in ('1050', '1070', '1071'):
-        offs = OffsetsFirmware_1050_1071()
-    elif args.fw == '1100':
+    if args.fw == '1100':
         offs = OffsetsFirmware_1100()
 
     exploit = Exploit(offs, args.interface, stage1, stage2)
