a4f27cadbd40f97c729d665510578bc74b527c03
SUIJUNG <40689080+SUIJUNG@users.noreply.github.com>
2024-05-15 23:26:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt install python3-scapy -y
 sudo apt install python3-tqdm -y
 sudo mv -f /root/PPPwn/rc.local /etc/rc.local
 sudo chmod +x /etc/rc.local
+sudo chmod +x /root/PPPwn/pppwn
 echo "*******************************************"
 echo -e '\033[36mInstall complete,\033[33m Rebooting\033[0m'
 echo "*******************************************"
