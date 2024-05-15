9f3bdec5dc06c200a1cb266698163e78fdabbce2
SUIJUNG <40689080+SUIJUNG@users.noreply.github.com>
2024-05-16 00:20:40 +0700

    Create install1.sh

---
+++ install1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Cloning the repository
+git clone --recursive https://github.com/SUIJUNG/PPPwn
+
+# Changing directory to the cloned repository
+cd PPPwn
+
+# Running the installation script
+sudo bash install.all
