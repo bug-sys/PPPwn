48e667b7f534d678212bb67cca8912e26aff4461
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-15 05:42:54 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-source config.ini
+watermark_name() {
+    echo -e "\033[1;35m"
+    cat << "EOF"
+88                                                                      
+88                                                                      
+echo88
+88,dPPYba,  88       88  ,adPPYb,d8    ,adPPYba, 8b       d8 ,adPPYba,  
+88P'    "8a 88       88 a8"    `Y88    I8[    "" `8b     d8' I8[    ""  
+88       d8 88       88 8b       88     `"Y8ba,   `8b   d8'   `"Y8ba,   
+88b,   ,a8" "8a,   ,a88 "8a,   ,d88    aa    ]8I   `8b,d8'   aa    ]8I  
+8Y"Ybbd8"'   `"YbbdP'Y8  `"YbbdP"Y8    `"YbbdP"'     Y88'    `"YbbdP"'  
+                         aa,    ,88                  d8'                
+                          "Y8bbdP"                  d8'               
+EOF
+    echo -e "\033[0m"
+    echo
+}
 
 check_interface() {
     if ! ip link show "$interface" | grep -q "state UP"; then
@@ -15,12 +31,13 @@ check_connection() {
             sudo shutdown -r now
             sleep 5
         fi
+        sleep 1
     done
 }
 
 run_pppwn() {
     while true; do
-    sudo "$pppwn" --interface "$interface" --fw "$fw" --stage1 "$stage1" --stage2 "$stage2" --timeout "$timeout" --wait-after-pin "$wait_after_pin" --groom-delay "$groom_delay" --buffer-size "$buffer_size" -a &
+        sudo "$pppwn" --interface "$interface" --fw "$fw" --stage1 "$stage1" --stage2 "$stage2" --timeout "$timeout" --wait-after-pin "$wait_after_pin" --groom-delay "$groom_delay" --buffer-size "$buffer_size" -a &
         pppwn_pid=$!
         check_connection &
         check_pid=$!
@@ -46,6 +63,8 @@ main_menu() {
     done
 }
 
+watermark_name
+source config.ini
 main_menu
 
 exit 0
