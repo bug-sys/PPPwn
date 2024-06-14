3342e7001c1d01cd85b00fcb1f85b71f022ca542
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-15 06:16:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,16 +3,14 @@
 watermark_name() {
     echo -e "\033[1;35m"
     cat << "EOF"
-88                                                                      
-88                                                                      
-88
-88,dPPYba,  88       88  ,adPPYb,d8    ,adPPYba, 8b       d8 ,adPPYba,  
-88P'    "8a 88       88 a8"    `Y88    I8[    "" `8b     d8' I8[    ""  
-88       d8 88       88 8b       88     `"Y8ba,   `8b   d8'   `"Y8ba,   
-88b,   ,a8" "8a,   ,a88 "8a,   ,d88    aa    ]8I   `8b,d8'   aa    ]8I  
-8Y"Ybbd8"'   `"YbbdP'Y8  `"YbbdP"Y8    `"YbbdP"'     Y88'    `"YbbdP"'  
-                         aa,    ,88                  d8'                
-                          "Y8bbdP"                  d8'               
+88888888ba   ad88888ba          ,d8      88        88 88888888888 888b      88  
+88      "8b d8"     "8b       ,d888      88        88 88          8888b     88  
+88      ,8P Y8,             ,d8" 88      88        88 88          88 `8b    88  
+88aaaaaa8P' `Y8aaaaa,     ,d8"   88      88aaaaaaaa88 88aaaaa     88  `8b   88  
+88""""""'     `"""""8b, ,d8"     88      88""""""""88 88"""""     88   `8b  88  
+88                  `8b 8888888888888    88        88 88          88    `8b 88  
+88          Y8a     a8P          88      88        88 88          88     `8888  
+88           "Y88888P"           88      88        88 88888888888 88      `888     bug-sys - 2024 (c)
 EOF
     echo -e "\033[0m"
     echo
