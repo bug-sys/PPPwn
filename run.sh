26c67926aaa1918c4a9f9842a3d4721faaac5f16
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-17 01:03:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ check_connection() {
 
 run_pppwn() {
     while true; do
-        sudo "$pppwn" --interface "$interface" --fw "$fw" --stage1 "$stage1" --stage2 "$stage2" --timeout "$timeout" --wait-after-pin "$wait_after_pin" --groom-delay "$groom_delay" --buffer-size "$buffer_size" -a &
+        sudo "$pppwn" --interface "$interface" --fw "$fw" --stage1 "$stage1" --stage2 "$stage2" --timeout "$timeout" --groom-delay "$groom_delay" -a &
         pppwn_pid=$!
         check_connection &
         check_pid=$!
