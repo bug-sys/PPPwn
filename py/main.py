f8bbbb095a443c4f66981ba673cf4e04c12d4d2d
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-22 06:41:00 +0700

    Update main.py

--- py/main.py
+++ py/main.py
@@ -1,69 +1,68 @@
 from argparse import ArgumentParser
-import psutil
 from exploit import *
 from offsets import *
 
-
 def main():
-    # Parser argumen untuk pppwn
-    global offs
-    parser = ArgumentParser('pppwn')
-    parser.add_argument('--fw', default='1100')
-    parser.add_argument('--interface', default=None)
-    parser.add_argument('--stage1', default=None)  # Atur default menjadi None
-    parser.add_argument('--stage2', default=None)  # Atur default menjadi None
+    parser = ArgumentParser(description='pppwn')
+    parser.add_argument('--interface', required=True)
+    parser.add_argument('--fw',
+                        choices=[
+                            '700','701','702','750', '751', '755',
+                            '800', '801', '803', '850', '852',
+                            '900', '903', '904', '950', '951', '960',
+                            '1000', '1001', '1050', '1070', '1071',
+                            '1100'
+                        ],
+                        default='1100')
+    parser.add_argument('--stage1', default='/root/PPPwn/1100/stage1.bin')
+    parser.add_argument('--stage2', default='/root/PPPwn/1100/stage2.bin')
     args = parser.parse_args()
 
-    print("\033[1;32mPorting by bug-sys - 2024 (c)\033[0m")
+    print("\033[1;32mPorting oleh bug-sys 2024 (c)\033[0m")
 
-    # Tentukan direktori dari main.py
-    current_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(args.stage1, mode='rb') as f:
+        stage1 = f.read()
 
-    # Tentukan jalur stage1 dan stage2
-    if args.stage1 is None:
-        args.stage1 = os.path.join(current_dir, 'stage1.bin')
-    if args.stage2 is None:
-        args.stage2 = os.path.join(current_dir, 'stage2.bin')
+    with open(args.stage2, mode='rb') as f:
+        stage2 = f.read()
+
+    if args.fw in ('700', '701', '702'):
+        offs = OffsetsFirmware_700_702()
+    elif args.fw in ('750', '751', '755'):
+        offs = OffsetsFirmware_750_755()
+    elif args.fw in ('800', '801', '803'):
+        offs = OffsetsFirmware_800_803()
+    elif args.fw in ('850', '852'):
+        offs = OffsetsFirmware_850_852()
+    elif args.fw == '900':
+        offs = OffsetsFirmware_900()
+    elif args.fw in ('903', '904'):
+        offs = OffsetsFirmware_903_904()
+    elif args.fw in ('950', '951', '960'):
+        offs = OffsetsFirmware_950_960()
+    elif args.fw in ('1000', '1001'):
+        offs = OffsetsFirmware_1000_1001()
+    elif args.fw in ('1050', '1070', '1071'):
+        offs = OffsetsFirmware_1050_1071()
+    elif args.fw == '1100':
+        offs = OffsetsFirmware_1100()
+    else:
+        raise ValueError("Versi firmware tidak didukung")
+
+    exploit = Exploit(offs, args.interface, stage1, stage2)
 
     while True:
-        with open(args.stage1, mode='rb') as f:
-            stage1 = f.read()
-
-        with open(args.stage2, mode='rb') as f:
-            stage2 = f.read()
-
-        if args.fw == '1100':
-            offs = OffsetsFirmware_1100()
-
-        selected_interface = args.interface
-
-        # Jika interface belum ditentukan secara manual, coba deteksi otomatis
-        if not selected_interface:
-            adapters = [interface for interface, addrs in psutil.net_if_addrs().items() if
-                        any(addr.family == psutil.AF_LINK for addr in addrs)]
-            for adapter in adapters:
-                if adapter in ['eth0', 'enp0s3', 'end0', 'Ethernet']:
-                    selected_interface = adapter
-                    break
-
-        if selected_interface:
-            print(f"\033[94mLAN Terdeteksi: {selected_interface}\033[0m")
-        else:
-            print("\033[91mTidak ada LAN yang sesuai dengan yang diharapkan.\033[0m")
-
-        # Buat objek exploit dan jalankan
-        exploit = Exploit(offs, selected_interface, stage1, stage2)
         try:
             exploit.run()
         except SystemExit as e:
-            if e.code != 1:
+            if e.code == 1:
+                continue
+            elif e.code == 0:
+                break
+            else:
                 raise
-            continue
-        break
 
     return 0
 
-
 if __name__ == '__main__':
-    # Keluar dengan kode keluaran utama
     exit(main())
